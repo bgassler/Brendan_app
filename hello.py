c0c8a34d048e1cb673059c10ec4f424f0c1b9c01
bgassler <bgassler4@gmail.com>
2014-07-24 19:25:57 -0400

    new commit

--- hello.py
+++ hello.py
@@ -1,10 +1,9 @@
-
-
-import os
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello():
-    return 'Hello World!'
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
